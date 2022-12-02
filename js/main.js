556357675f7a273a3e1641fe7179825020045fa5
Dmytro Boiarchuk <116515941+DmytroBoiarchuk@users.noreply.github.com>
2022-12-02 17:14:04 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
-    $(".owl-carousel").owlCarousel();
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+        center: true,
+        margin: 700,
+        loop:true,
+        items:1,
+})
+    $('.bot-arrow').click(function() {
+        owl.trigger('next.owl.carousel', [2000]);
+    })
+
+    $('.top-arrow').click(function() {
+        owl.trigger('prev.owl.carousel', [2000]);
+    })
 });
